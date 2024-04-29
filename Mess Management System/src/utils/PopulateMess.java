330c132b0a4ad358d7ea9feacd83ac951ec6cb2b
Pratik Warade <148488693+WaradePratik@users.noreply.github.com>
2024-04-29 20:58:24 +0530

    Update PopulateMess.java

--- Mess Management System/src/utils/PopulateMess.java
+++ Mess Management System/src/utils/PopulateMess.java
@@ -12,10 +12,10 @@ public class PopulateMess {
 		 * int customerId, String firstName, String lastName, String email, String passsword, String address,
 			LocalDate registerDate,LocalDate lastDate, String phoeNo, Plan plan,double fees
 		 */
-		mess.add(new Mess(111, "kunal", "patil", "kunal.dac@iacsd.com", "pass", "akurdi", LocalDate.parse("2023-12-09"),LocalDate.parse("2024-12-09") , "8551913732", Plan.valueOf("YEAR"), 32000));
-		mess.add(new Mess(112, "rohit", "balpande", "rohit.dac@iacsd.com", "pass1", "akurdi", LocalDate.parse("2023-12-09"),LocalDate.parse("2024-12-09") , "8576913732", Plan.valueOf("HALFYEAR"), Plan.HALFYEAR.getFees()));
-		mess.add(new Mess(113, "shirish", "nalawade", "shirish.dac@iacsd.com", "pass12", "akurdi", LocalDate.parse("2024-02-09"),LocalDate.parse("2024-12-09") , "8551910932", Plan.QUARTERLY, Plan.QUARTERLY.getFees()));
-		mess.add(new Mess(114, "mukul", "khot", "mukul.dac@iacsd.com", "pass123", "akurdi", LocalDate.parse("2023-12-09"),LocalDate.parse("2024-12-09") , "9151913732", Plan.MONTHLY, Plan.MONTHLY.getFees()));
+		mess.add(new Mess(111, "sandesh", "patil", "sandesh.dac@iacsd.com", "pass", "akurdi", LocalDate.parse("2023-12-09"),LocalDate.parse("2024-12-09") , "8551913732", Plan.valueOf("YEAR"), 32000));
+		mess.add(new Mess(112, "tanmay", "navale", "tanmay.dac@iacsd.com", "pass1", "akurdi", LocalDate.parse("2023-12-09"),LocalDate.parse("2024-12-09") , "8576913732", Plan.valueOf("HALFYEAR"), Plan.HALFYEAR.getFees()));
+		mess.add(new Mess(113, "pratik", "warade", "pratik.dac@iacsd.com", "pass12", "akurdi", LocalDate.parse("2024-02-09"),LocalDate.parse("2024-12-09") , "8551910932", Plan.QUARTERLY, Plan.QUARTERLY.getFees()));
+		mess.add(new Mess(114, "arjun", "bharara", "arjun.dac@iacsd.com", "pass123", "akurdi", LocalDate.parse("2023-12-09"),LocalDate.parse("2024-12-09") , "9151913732", Plan.MONTHLY, Plan.MONTHLY.getFees()));
 		return mess;
 	}
 }
